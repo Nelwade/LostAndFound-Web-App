f043c07cef00e0404d41dff44d297f9487bcc2bc
Nelwade <nelsonowade@gmail.com>
2022-04-19 16:14:58 +0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import url
+
+from django.urls import include, re_path
 from .import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
-    url(r'^register/$',views.register,name='register'),
-    url(r'^profile/$',views.profile,name='profile'),
-    url(r'^request/$',views.requestItem,name='request_item'),
-    url(r'^profile/edit$', views.editprofile, name='edit_profile'),
-    url(r'^password/$', views.change_password, name='change_password'),
-    url(r'^claim/(?P<id>\d+)/$', views.claim, name='claim'),
+    re_path(r'^$', views.home, name='home'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    re_path(r'^register/$',views.register,name='register'),
+    re_path(r'^profile/$',views.profile,name='profile'),
+    re_path(r'^request/$',views.requestItem,name='request_item'),
+    re_path(r'^profile/edit$', views.editprofile, name='edit_profile'),
+    re_path(r'^password/$', views.change_password, name='change_password'),
+    re_path(r'^claim/(?P<id>\d+)/$', views.claim, name='claim'),
 ]
